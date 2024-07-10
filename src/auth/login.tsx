acc28912b246fe875fac6a4912f1a9cc27e1d943
Croitor Gheorghita <sebi.croitor@yahoo.com>
2024-07-10 12:24:28 +0300

    first commit

---
+++ src/auth/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../components/ui/button'
+
+const login = () => {
+  return (
+    <div >
+        <Button>Apasa aic</Button>
+    </div>
+  )
+}
+
+export default login
